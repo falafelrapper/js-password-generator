fb21eac2b40cf0ce8a2ebf4b1d6a0417c5ad32b0
Zachary Roy <vgd199@gmail.com>
2023-12-18 20:23:47 -0800

    Reworked my for loop, which now returns a proper randomly generated password. Site is ready for deployment.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# js-password-generator
+# Fullstack Development Challenge 3
+
+## Welcome to the third challenge for the UCLA edX full stack development bootcamp! 
+
+In this challenge we were supposed to rework a starter Javascript code so that it would generate a random password. For this to work, we needed to be able to ask what criteria it would need, and from there make a new password only using the criteria selected. Lets start with our user story and acceptance criteria:
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## The Completed Challenge
+As you can see below, we now have a completed password generator with the following criteria now completed, which you can see below:
+
+![alt text](./assets/images/password-generator.png "Completed Password Generator")
+
+To visit this site and try out all the different combinations, you may click [here](https://falafelrapper.github.io/js-password-generator/)! Feel free to try different combinations of passwords to generate!
+
+## Credits
+This refactor was completed by Zachary Roy with help from Bryan Swarthout via Office Hours and tutoring from Andrew Tirpok.
--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-/*
-1X. Click generate password to generate prompts
-2X. Make them all confirmations to select the criteria needed for password
-3X. Make a prompt for password length
-4. Make validations for all criteria that will go into the generator
-5. When every prompt is answered for and at least one criteria is confirmed, generate password
-6. Display password on page
 
-extras
-// i need a for loop for the final password, which add the password and possibleChar
-// adding each character to our arrays
-//
-*/
-
+// Arrays for our variables
 var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialChara = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var possibleChar = [];
 
-// Write password to the #password input
 function generatePassword(){
 
   var finalPass = "";
+
+  // Prompt window for our password's length
   var passwordLength = prompt("Please type a password length between 8 and 128:");
   if (passwordLength < 8 || passwordLength > 128){
     window.alert("Please make sure your password is between 8 and 128.");
     return;
   }
 
+  // Confirm windows for each of our variables ie lowercase, uppercase, special characters, and numerics
   var lowerCase = confirm("Would you like your password to have lowercase characters?");
   if (lowerCase === true){
     possibleChar = possibleChar.concat(lowerCaseChar);
   }
-  console.log(possibleChar);
 
   var upperCase = confirm("Would you like your password to have uppercase characters?");
   if (upperCase === true){
     possibleChar = possibleChar.concat(upperCaseChar);
   }
-  console.log(possibleChar);
 
   var specialChar = confirm("Would you like your password to have special characters?");
   if (specialChar === true){
     possibleChar = possibleChar.concat(specialChara);
   }
-  console.log(possibleChar);
 
   var numeric = confirm("Would you like your password to have numeric characters?");
   if (specialChar === true){
     possibleChar = possibleChar.concat(numericChar);
   }
-  console.log(possibleChar);
 
-  finalPass = finalPass + possibleChar[Math.floor(Math.random()*possibleChar.length)];
-
-  for (var i = 0; i < passwordLength.length; i++){
-    finalPass + possibleChar[Math.floor(Math.random()*passwordLength.length)]
+  // For loop that runs the program so that it will make sure our password is randomly generated with the arrays we have chosen
+  for (var i = 0; i < passwordLength; i++){
+    finalPass += possibleChar[Math.floor(Math.random()*possibleChar.length)];
   }
 
-  console.log(finalPass);
+  // The final password is then returned onto the main display
   return finalPass;
 
 }
@@ -73,5 +58,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
